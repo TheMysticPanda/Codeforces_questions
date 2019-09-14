19d7b267244a27f4ae8eb288b47c6c9b8508383b
TheMysticPanda <47250771+TheMysticPanda@users.noreply.github.com>
2019-09-14 00:20:22 -0400

    changed_file

--- make_product_equal_one.py
+++ make_product_equal_one.py
@@ -1,33 +1,33 @@
 #make product equal to one
 
-total_number_of_numbers=int(input())
-
+user_input=int(input())
 list_of_ints=input().split()
 for i in range(len(list_of_ints)):
   list_of_ints[i]=int(list_of_ints[i])
 
 number_of_negatives=0
 number_of_positives=0
+number_of_zeroes=0
 coins_needed=0
 
 for i in range(len(list_of_ints)):
   if list_of_ints[i]<0:
     number_of_negatives+=1
-    while list_of_ints[i]!=-1:
-      list_of_ints[i]+=1
-      coins_needed+=1
+    coins_needed+=((list_of_ints[i]*-1)-1)
   elif list_of_ints[i]>0:
     number_of_positives+=1
-    while list_of_ints[i]!=1:
-      list_of_ints[i]-=1
-      coins_needed+=1
+    coins_needed+=(list_of_ints[i]-1)
   else:
-    number_of_positives+=1
-    coins_needed+=1
+    number_of_zeroes+=1
 
-if (number_of_negatives%2)!=0:
-  if number_of_positives>=1:
+#case when all inputs are either 1 or -1
+
+if (number_of_zeroes==0):
+  if (number_of_negatives%2)!=0:
     coins_needed+=2
+else:
+  coins_needed+=number_of_zeroes
 
 print(coins_needed)
+
   
