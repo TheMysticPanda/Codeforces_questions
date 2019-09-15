47afc98fb4d9cd836513e8f2a0599bb29b726c9e
TheMysticPanda <47250771+TheMysticPanda@users.noreply.github.com>
2019-09-15 19:10:46 -0400

    help with this please!
    This code runs indefinitely if number of queries is too long!

---
+++ book_reading_question.py
@@ -0,0 +1,47 @@
+number_of_queries=int(input())
+
+resulting_values=[]
+bigger_list=[]
+
+for i in range(number_of_queries):
+  list_of_data=input().split()
+  for i in range(2):
+    list_of_data[i]=int(list_of_data[i])
+  bigger_list.append(list_of_data)
+    
+  
+
+
+for smaller_list in bigger_list:
+  summation=0
+  
+  numberOfPages=int(smaller_list[0])
+  RequiredDivisor=int(smaller_list[1])
+
+  first_ten_last_digits=[None]*10
+
+  for i in range(10):
+    first_ten_last_digits[i]=(RequiredDivisor*(i+1))%10
+ 
+  
+  numberoftimes=numberOfPages//RequiredDivisor
+
+  counter=0
+  for i in range(numberoftimes):
+    summation+=(first_ten_last_digits[counter])
+    counter=(counter+1)%10
+
+  resulting_values.append(summation)
+
+
+print(resulting_values)
+    
+    
+  
+  
+
+  
+  
+
+  
+  
