720ef18043f600538179f193ef0a5772f0b0ec92
Oishika <oc618@nyu.edu>
2021-09-08 02:19:49 -0400

    more graph algorithms

--- DataStructureImplementations/GraphUndirected.py
+++ DataStructureImplementations/GraphUndirected.py
@@ -1,10 +1,15 @@
+#importing queue for BFS
+import queue
+from operator import itemgetter
+
 #Undirected graph
 class Graph:
     class Node:
         def __init__(self, data):
             self.data = data
-            #each item in self.neighbours is a list with two items [name_of_neighbour_node, weight]
+            #each item in self.neighbours is a list with two items [neighbour_node, weight]
             self.neighbours = []
+        
 
     def __init__(self, v = 0):
         #number of vertices
@@ -12,6 +17,7 @@ class Graph:
         #adjacency list
         self.nodes = {}
 
+    #adds an edge between node1, and node2
     def add_edge(self, node1, node2, weight = 0):
         node1.neighbours.append([node2,weight])
         node2.neighbours.append([node1,weight])
@@ -22,6 +28,12 @@ class Graph:
             self.v += 1 
             self.nodes[node2] = node2.neighbours
     
+    #deletes edge between node1 and node2
+    def remove_edge(self, node1, node2, weight):
+        node1.neighbours.remove([node2,weight])
+        node2.neighbours.remove([node1, weight])
+
+    #TODO: remove nodes 
 
     def print_graph(self):
         for key in self.nodes:
@@ -30,11 +42,137 @@ class Graph:
                 print([neighbour[0].data, neighbour[1]],end = " ")
             print("\n")
 
-    def dfs(self):
+    def dfs(self, starting_node, visited = set()):
+        if starting_node not in visited:
+            visited.add(starting_node)
+            print(starting_node.data)
+            for children in self.nodes[starting_node]:
+                self.dfs(children[0],visited)
 
-    def bfs(self):
+    def bfs(self, starting_node):
+        node_queue = queue.Queue()
+        visited = set()
 
+        node_queue.put(starting_node)
+        visited.add(starting_node)
+
+        while not node_queue.empty():
+            node_exploring_now = node_queue.get()
+            print(node_exploring_now.data)
+            visited.add(node_exploring_now)
+            for neighbour in node_exploring_now.neighbours:
+                if neighbour[0] not in visited:
+                    node_queue.put(neighbour[0])
+
+    def detect_cycle_bfs(self):
+        node_queue = queue.Queue()
+        visited_nodes = {}
+
+        for node in self.nodes:
+            visited_nodes[node] = -1
+
+        for node in self.nodes:
+            node_queue.put(node)
+            visited_nodes[node] = 0
+            break
+        
+       
+        while not node_queue.empty():
+            visited_node = node_queue.get()
+            visited_nodes[visited_node] = 1
+            for each_neighbour in visited_node.neighbours:
+                if visited_nodes[each_neighbour[0]] == 0:
+                    return True
+                elif visited_nodes[each_neighbour[0]] != 1:
+                    node_queue.put(each_neighbour[0])
+                    visited_nodes[each_neighbour[0]] = 0
+        return False
+                    
     
+    def detect_cycle_dfs(self, parent, starting_node, visited, unvisited):
+        if starting_node in visited:
+            return True
+        else:
+            visited.add(starting_node)
+            unvisited.remove(starting_node)
+            for child in starting_node.neighbours:
+                if child[0] != parent:
+                    cycle = self.detect_cycle_dfs(starting_node, child[0], visited, unvisited)
+                    if cycle is True:
+                        return True
+            return False
+
+
+
+
+    def detect_cycle(self):
+        visited_nodes = set()
+        unvisited_nodes = set()
+        for node in self.nodes:
+            unvisited_nodes.add(node)
+
+        starting_node = None
+        for node in self.nodes:
+            starting_node = node
+            break
+
+        return self.detect_cycle_dfs(starting_node, starting_node, visited_nodes, unvisited_nodes)
+
+    def prims_algorithm(self):
+        edges = []
+        visited = set()
+        unvisited = set()
+        for each_node in self.nodes:
+            unvisited.add(each_node)
+        
+        #randomly choses a node to be added to visited as a starting node
+        for node in self.nodes:
+            visited.add(node)
+            unvisited.remove(node)
+            break
+
+        #Actual Prim's algorithm
+        while len(unvisited) > 0:
+            minimum_distance = float('inf')
+            next_node = None
+            next_edge = None
+            for node in visited:
+                for each_neighbour in node.neighbours:
+                    if each_neighbour[0] not in visited and each_neighbour[1] < minimum_distance:
+                        minimum_distance = each_neighbour[1]
+                        next_node = each_neighbour[0]
+                        next_edge = (node, each_neighbour[0])
+            unvisited.remove(next_node)
+            visited.add(next_node)
+            edges.append(next_edge)
+        tuple_representation = []
+        for edge in edges:
+            tuple_representation.append((edge[0].data, edge[1].data))
+        return tuple_representation
+
+    # def kruskals_algorithm(self):
+    #     new_tree = Graph()
+    #     new_tree.v = self.v
+    #     new_tree.nodes = {}
+    #     unsorted_edges = []
+
+    #     for node in self.nodes:
+    #         for neighbour in self.nodes[node]:
+    #             edge = [node, neighbour[0], neighbour[1]]
+    #             unsorted_edges.append(edge)
+        
+    #     sorted_edges = sorted(unsorted_edges, key=itemgetter(2))
+    #     for i in range(-1, -len(sorted_edges),-1):
+    #         new_tree.add_edge(sorted_edges[i][0], sorted_edges[i][1], sorted_edges[i][2])
+    #         if new_tree.detect_cycle_bfs():
+    #             new_tree.remove_edge(sorted_edges[i][0], sorted_edges[i][1], sorted_edges[i][2])
+
+    #     tuple_representation = []
+    #     for node in new_tree.nodes:
+    #         for neighbour in node.neighbours:
+    #             tuple_representation.append([node.data, neighbour.data])
+
+    #     return tuple_representation
 
 
 a = Graph.Node("a")
@@ -42,12 +180,25 @@ b = Graph.Node("b")
 c = Graph.Node("c")
 d = Graph.Node("d")
 e = Graph.Node("e")
+f = Graph.Node("f")
+g = Graph.Node("g")
 graph = Graph()        
-graph.add_edge(a,b,5)
-graph.add_edge(b,e,4)
+graph.add_edge(a,b,2)
+graph.add_edge(a,c,3)
 graph.add_edge(a,d,3)
-graph.add_edge(c,e,9)
-graph.print_graph()
+graph.add_edge(b,c,4)
+graph.add_edge(b,e,3)
+graph.add_edge(c,e,1)
+graph.add_edge(c,d,5)
+graph.add_edge(d,f,7)
+graph.add_edge(c,f,6)
+graph.add_edge(f,e,8)
+graph.add_edge(f,g,9)
+
+# graph.print_graph()
+# graph.dfs(a)
+# graph.bfs(a)
+print(graph.detect_cycle_bfs())
 
         
 
